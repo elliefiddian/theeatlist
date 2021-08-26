669440b4f89d0af6c2dd342246df7195cab94db4
elliefiddian <86102143+elliefiddian@users.noreply.github.com>
2021-08-26 12:25:28 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contacts';
+import Service from './Pages/Service/Service';
+import Navbar from './Components/Navbar/Navbars';
+
+
+//move to other pages//
+    const App = () => {
+      return (
+       <Router>
+        <Navbar/>
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Home/>
+            </Route>
+            <Route path="/about" exact>
+              <About/>
+            </Route>
+            <Route path="/service" exact>
+              <Service/>
+            </Route>
+            <Route path="/contact" exact>
+              <Contact/>
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </main>
+       </Router>
+      );
+    }
+    
+    export default App;
+    
